7b42a4014e4cdd9fedc6eba51cb45e519667a65f
X3n0m0rph59 <x3n0m0rph59@gmail.com>
2013-12-05 18:30:39 +0100

    First working version

---
+++ videores.py
@@ -0,0 +1,21 @@
+#!/bin/env python
+
+from ffvideo import VideoStream
+from glob import iglob
+from os import getcwd, chdir
+import sys
+
+def print_usage():
+   pass
+
+
+def enum_files(dir):
+   chdir(dir)
+   for file in iglob("*.mkv"):
+      vs = VideoStream(file)
+      print "Res: %dx%d\tFile: %s" % (vs.frame_width, vs.frame_height, file)
+
+if __name__ == "__main__":
+   print("videores.py")
+   enum_files(sys.argv[1])
+
