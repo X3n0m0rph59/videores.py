748bd1d49aa0a77ffb93ac037b053c38dc34be52
X3n0m0rph59 <x3n0m0rph59@gmail.com>
2013-12-07 15:08:11 +0100

    Corrected indentation

--- videores.py
+++ videores.py
@@ -6,16 +6,16 @@ from os import getcwd, chdir
 import sys
 
 def print_usage():
-   pass
+    pass
 
 
 def enum_files(dir):
-   chdir(dir)
-   for file in iglob("*.mkv"):
-      vs = VideoStream(file)
-      print "Res: %dx%d\tFile: %s" % (vs.frame_width, vs.frame_height, file)
+    chdir(dir)
+    for file in iglob("*.mkv"):
+        vs = VideoStream(file)
+        print "Res: %dx%d\tFile: %s" % (vs.frame_width, vs.frame_height, file)
 
 if __name__ == "__main__":
-   print("videores.py")
-   enum_files(sys.argv[1])
+    print("videores.py")
+    enum_files(sys.argv[1])
 
